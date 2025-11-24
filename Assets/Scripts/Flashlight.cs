fc44d5001503bc388f1c78ab97823c95ae3c8132
Heeme Beeme <heemebeeme@gmail.com>
2025-11-24 09:29:09 -0400

    door anim + flashlight fix + trigger position change

--- Assets/Scripts/Flashlight.cs
+++ Assets/Scripts/Flashlight.cs
@@ -10,63 +10,28 @@ public class Flashlight : MonoBehaviour
     float dimTime = 0.5f;
     [SerializeField]
     (float, float) flashlightMinMax= (0.3f, 5);
-    [SerializeField]
-    Light flashlight;
+
+    public Light flashlight;
 
     public AudioClip FlashlightOn;
     public AudioClip FlashlightOff;
 
     public bool lightActivity = true;
-
-    void Start()
-    {
-        flashlight = GetComponent<Light>();
-    }
     void Update()
     {
         RaycastHit hit;
         Ray raycast = new Ray(transform.position, transform.forward);
-        if (lightActivity)
+
+        if (Physics.Raycast(raycast, out hit))
         {
-            if (Physics.Raycast(raycast, out hit))
+            raycastDistance = hit.distance - 0.3f;
+
+            if (raycastDistance > flashlightMinMax.Item2)
             {
-                raycastDistance = hit.distance - 0.3f;
-
-                if (raycastDistance > flashlightMinMax.Item2)
-                {
-                    raycastDistance = flashlightMinMax.Item2;
-                }
-
-                flashlight.intensity = Mathf.Clamp(Mathf.Lerp(flashlight.intensity, raycastDistance, dimTime), flashlightMinMax.Item1, flashlightMinMax.Item2);
-
-                if (Input.GetKeyDown(KeyCode.F))
-                {
-                    lightActivity = false;
-                    flashlight.intensity = 0;
-                    GameManager.Instance.FlashOffSource.Play();
-                }
+                raycastDistance = flashlightMinMax.Item2;
             }
+
+            flashlight.intensity = Mathf.Clamp(Mathf.Lerp(flashlight.intensity, raycastDistance, dimTime), flashlightMinMax.Item1, flashlightMinMax.Item2);
         }
-        else
-        {
-            if (Input.GetKeyDown(KeyCode.F))
-            {
-                Physics.Raycast(raycast, out hit);
-
-                raycastDistance = hit.distance - 0.3f;
-
-                if (raycastDistance > flashlightMinMax.Item2)
-                {
-                    raycastDistance = flashlightMinMax.Item2;
-                }
-
-                flashlight.intensity = Mathf.Clamp(Mathf.Lerp(flashlight.intensity, raycastDistance, dimTime), flashlightMinMax.Item1, flashlightMinMax.Item2);
-
-                lightActivity = true;
-                GameManager.Instance.FlashOnSource.Play();
-            }
-        }
-       
-        
     }
 }
