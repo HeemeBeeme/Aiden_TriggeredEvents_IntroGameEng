8121ec19d0b488951ddae443d589cf3a436f76e2
Heeme Beeme <heemebeeme@gmail.com>
2025-11-25 13:23:02 -0400

    enemy finished

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UIElements;
 
 public class EnemyAI : MonoBehaviour
 {
     public Transform Target;
-
     public NavMeshAgent agent;
+
+    public float rotateSpeed = 10;
     void Update()
     {
         agent.SetDestination(Target.position);
